e6d83ed919619371ef4994bed927d7987b305262
Owner <RajKSidhu>
2025-05-22 00:13:59 +1200

    Updated properties file

--- src/main/java/com/parser/Constants.java
+++ src/main/java/com/parser/Constants.java
@@ -9,10 +9,10 @@ public interface Constants {
 	String RESPONSE_FILTER = "Response";
 	String API_FORMAT = "API: %s";
 	String RESPONSE_TIME_FORMAT = "Response Time: %s (ms)";
-	String REQUEST_RESPONSE_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-dhanistockorchestration-request-response.log.2021-10-26";
-	String WEBLOGIC_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-dhanistockorchestration-weblogic-api.log.2021-10-26";
-	String TR_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-dhanistockorchestration-tr-api.log.2021-10-26";
-	String DION_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-dhanistockorchestration-dion-api.log.2021-10-26";
+	String REQUEST_RESPONSE_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-request-response.log.2021-10-26";
+	String WEBLOGIC_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-weblogic-api.log.2021-10-26";
+	String TR_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-tr-api.log.2021-10-26";
+	String DION_FILE = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\filtered-dion-api.log.2021-10-26";
 	String OUTPUT_FILE_PATH = "C:\\Users\\rajdeep\\Documents\\Prod Logs\\Parsed\\parsed-log-%s.xlsx";
 	String SHEET_NAME = "Logs";
 	int RESPONSE_TIME_LIMIT = 1500;
